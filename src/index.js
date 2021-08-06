59b2b80fc0a52a1fd5ebeb0eafb5b724f6957840
Robbie-McGregor <40312007+Robbie-McGregor@users.noreply.github.com>
2021-08-06 21:57:07 +1200

    Section Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let date = new Date();
+let year = date.getFullYear();
+
+ReactDOM.render(
+  <>
+    <p>Created by Robbie McGregor</p>
+    <p>Copyright {year}</p>
+  </>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
